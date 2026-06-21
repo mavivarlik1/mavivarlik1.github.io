aefc1ff41ebe54c618e3abd1a6173b76217b18ac
mavivarlik1 <supralanderxbox@gmail.com>
2026-06-21 09:26:30 +0300

    Update privacy.js

--- privacy.js
+++ privacy.js
@@ -1,49 +1,19 @@
-// privacy.js - CoreBase Yasal Gizlilik Politikası Veri Seti
+// privacy.js
 export const privacyData = {
     tr: {
-        title: "Gizlilik Politikası",
-        date: "Son Güncelleme: 20 Haziran 2026",
+        title: "Kapsamlı Gizlilik ve Çerez Politikası",
+        date: "Son Düzenleme: Haziran 2026",
         content: `
-            <p>CoreBase olarak, corebase.com.tr adresindeki web sitemizi ziyaret eden kullanıcılarımızın gizliliğine büyük önem veriyoruz. Bu Gizlilik Politikası belgesi, topladığımız bilgi türlerini ve bunları nasıl kullandığımızı açıklamaktadır.</p>
-            
-            <h4>1. Bilgi Toplama ve Kullanımı</h4>
-            <p>CoreBase forumunda kullanıcı deneyimini artırmak, kayıt işlemlerini gerçekleştirmek ve güvenliği sağlamak amacıyla e-posta adresi ve kullanıcı adı gibi temel bilgileri toplayabiliriz. Bu bilgiler hiçbir şekilde üçüncü şahıslarla paylaşılmaz.</p>
-            
-            <h4>2. Log Dosyaları</h4>
-            <p>CoreBase, standart bir prosedür olarak log dosyalarını kullanır. Bu dosyalar ziyaretçilerin IP adreslerini, tarayıcı türlerini, İnternet Servis Sağlayıcılarını (ISS), tarih/saat damgalarını ve yönlendirme sayfalarını kaydeder. Bu veriler kişisel olarak tanımlanabilir herhangi bir bilgiyle bağlantılı değildir.</p>
-            
-            <h4>3. Çerezler ve Web İşaretçileri</h4>
-            <p>Ziyaretçilerin tercihlerini ve web sitesinde eriştikleri sayfaları depolamak, içeriği kullanıcıların tarayıcı türüne göre optimize etmek amacıyla standart çerezler (cookies) kullanılmaktadır.</p>
-            
-            <h4>4. Google AdSense ve Reklam Politikaları</h4>
-            <p>Sitemizde Google AdSense reklam modeli kullanılmaktadır. Google, üçüncü taraf bir satıcı olarak sitemizde reklam yayınlamak için çerezlerden yararlanır. Google'ın DART çerezini kullanması, kullanıcılarımıza sitemizi ve internetteki diğer siteleri ziyaretlerine dayalı olarak reklam sunmasına olanak tanır.</p>
-            <p>Kullanıcılar, <a href="https://policies.google.com/technologies/ads" target="_blank" style="color:#38bdf8;">Google Reklam ve İçerik Ağı gizlilik politikasını</a> ziyaret ederek DART çerezinin kullanılmasını tamamen engelleyebilirler.</p>
-            
-            <h4>5. İletişim</h4>
-            <p>Gizlilik politikamız hakkında sorularınız için resmi destek hattımız: <b>supralanderxbox@gmail.com</b></p>
+            <b>1. Veri Toplama Politikası</b><br>CoreBase ekosistemi, kullanıcıların tarayıcı oturumlarını anonim kimliklerle güvenceye alır. Toplanan veriler tamamen izole biçimde sunucularda saklanır.<br><br>
+            <b>2. Reklam Ortakları ve Çerezler (AdSense Uyum)</b><br>Sitemiz, Google AdSense reklam ağını barındırır. Google, kullanıcıların ilgi alanlarına göre reklam sunmak amacıyla DoubleClick DART çerezlerini kullanmaktadır. Reklam ağından çıkmak isteyen kullanıcılar, Google gizlilik politikası üzerinden çerez izinlerini kapatabilirler.
         `
     },
     en: {
-        title: "Privacy Policy",
-        date: "Last Updated: June 20, 2026",
+        title: "Global Privacy & Cookie Policy",
+        date: "Last Updated: June 2026",
         content: `
-            <p>At CoreBase, accessible from corebase.com.tr, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by CoreBase and how we use it.</p>
-            
-            <h4>1. Information Collection and Use</h4>
-            <p>For a better experience while using our Forum, we may require you to provide us with certain personally identifiable information, including username and email address. The information will be retained securely.</p>
-            
-            <h4>2. Log Files</h4>
-            <p>CoreBase follows a standard procedure of using log files. These files log visitors when they visit websites. The information collected by log files includes internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, and referring/exit pages.</p>
-            
-            <h4>3. Cookies and Web Beacons</h4>
-            <p>Like any other website, CoreBase uses "cookies". These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited.</p>
-            
-            <h4>4. Google AdSense and Advertising Cookies</h4>
-            <p>Google is one of the third-party vendors on our site. It uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to corebase.com.tr and other sites on the internet.</p>
-            <p>Visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL: <a href="https://policies.google.com/technologies/ads" target="_blank" style="color:#38bdf8;">https://policies.google.com/technologies/ads</a></p>
-            
-            <h4>5. Contact Us</h4>
-            <p>If you have additional questions regarding our privacy rules, feel free to contact us at: <b>supralanderxbox@gmail.com</b></p>
+            <b>1. Data Practices</b><br>CoreBase ecosystem secures anonymous browser sessions. All stored file assets are private and sandboxed.<br><br>
+            <b>2. AdSense Compliance</b><br>This platform runs Google AdSense integrations. Google utilizes DART cookies to customize advertisements based on your network footprints. Users can modify custom tracking properties via official Google network panels.
         `
     }
 };
